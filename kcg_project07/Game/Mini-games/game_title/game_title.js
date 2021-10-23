36c8076d2197a451ab7b15d5a64ac0ba2f105c54
e20c2007-sm <86354855+e20c2007-sm@users.noreply.github.com>
2021-10-23 15:28:41 +0900

    commit

--- kcg_project07/Game/Mini-games/game_title/game_title.js
+++ kcg_project07/Game/Mini-games/game_title/game_title.js
@@ -1,3 +1,22 @@
+let gameList;
+
+function getGameList(){
+    let req = new XMLHttpRequest();
+
+    req.open("get", "../csv/game_list.csv", true);
+    req.send(null);
+
+    req.onload = function(){
+        csvToArray(req.responseText);
+        alert("処理を終了")
+    }
+}
+
+function csvToArray(data){
+    let array = data.split("\n");
+
+    alert(array)
+}
 
 //初期化
 $(function(){
