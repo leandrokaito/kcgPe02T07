f512998a4d97f4143b6f8bbe1f36ec4153e57b00
e20c2007-sm <86354855+e20c2007-sm@users.noreply.github.com>
2021-11-16 14:58:00 +0900

    c

--- kcg_project07/Game/Mini-games/FastHit/js/index.js
+++ kcg_project07/Game/Mini-games/FastHit/js/index.js
@@ -2,8 +2,15 @@
 
 $(function(){
 
-    $("#front_view_box").fadeIn(500).animate({
-        width: "120%",
-        height: "120%"
-    },3000);
+    $("#front_view_box").fadeIn(500);
+    $("#front_view").addClass("scale_up");
+
+    setTimeout(function(){
+        $("#front_view").fadeOut(500).css({
+            width: "120%",
+            height: "120%"
+        });
+        $("#shadow_frame #front_view").fadeIn(500);
+    }, 2500);
+
 });
