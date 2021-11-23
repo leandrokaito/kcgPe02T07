14445e3c52c29cc1b6dcd5edd3ae012e2282afc1
e20c2007-sm <86354855+e20c2007-sm@users.noreply.github.com>
2021-11-23 12:05:44 +0900

    c

--- kcg_project07/Game/Mini-games/FastHit/js/fastHit.js
+++ kcg_project07/Game/Mini-games/FastHit/js/fastHit.js
@@ -1,11 +1,52 @@
 
+
+/* アニメーション関連 */
+function personMove(person, key){
+    switch(key){
+        case "pop":
+            person.addClass("poped");
+            setTimeout(function(){
+                person.css({
+                    "width": "20rem",
+                    "height": "20rem",
+                    "top": 0,
+                    "transform": "scale(1)"
+                });
+            }, 300);
+            break;
+        
+        case "push":
+            person.removeClass("poped").addClass("push");
+            setTimeout(function(){
+                person.css({
+                    "width": 0,
+                    "height": 0,
+                    "top": "20rem",
+                    "transform": "scale(0)"
+                });
+                person.removeClass("push");
+            }, 300);
+            break;
+    }
+}
+
 $(function(){
     $("#play_screen").fadeIn(500);
 
-    $("#play_screen").on("mousemove", function(e){
+    setTimeout(function(){
+        personMove($(".tutorial .person"), "pop");
+    }, 5000);
+
+    $(document).on("mousemove","#play_screen", function(e){
         $("#aim_pointer").css({
             "top": `${e.clientY}px`,
-            "left": `${e.clientX}px`
+            "left": `${e.clientX}px`,
+            "transform": "none"
         });
+    });
+
+    $(document).on("click", ".poped", function(){
+        personMove($(this), "push");
+        return false;
     });
 });
